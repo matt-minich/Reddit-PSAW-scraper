ca7301297bc34e2cd4681f1ed2e86c468aab54ad
Matt Minich <mminich@wisc.edu>
2020-08-21 19:36:01 -0600

    Update Reddit_PSAW.py
    Added end_epoch and some workarounds for a bug (several submissions were stored without selftext or is_original_content attributes. This script contains a workaround for that).

--- Reddit_PSAW.py
+++ Reddit_PSAW.py
@@ -2,34 +2,48 @@ from psaw import PushshiftAPI
 import csv
 import datetime as dt
 
+
 api = PushshiftAPI()
 
-start_epoch=int(dt.datetime(2012, 11,9).timestamp())
+start_epoch=int(dt.datetime(2018, 1,1).timestamp())
+end_epoch=int(dt.datetime(2019,1,1).timestamp())
 
-corpus = list(api.search_submissions(after = start_epoch,
-                                    subreddit = 'trees',
-                                     limit = None))
+subreddit = input('Which subreddit would you like to scrape? ')
 
-with open('D:/CAMER/newtrees.csv','w', encoding = 'utf-8') as csvfile:
-    fieldnames = ['id','title','author','original content','selfpost','stickied','selftext','comment forest','time created','locked','number of comments','NSFW','permalink','score','upvote ratio','url']
-    filewriter = csv.DictWriter(csvfile, fieldnames = fieldnames)
+
+with open('D:/CAMER/%s.csv' % subreddit, 'w', encoding='utf-8') as csvfile:
+    fieldnames = ['id', 'title', 'author', 'original content', 'selfpost', 'stickied', 'selftext',
+                  'time created', 'locked', 'number of comments', 'NSFW', 'permalink', 'score',
+                  'url']
+    filewriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
     filewriter.writeheader()
 
-    for i in corpus:
+    for i in api.search_submissions(after=start_epoch,
+                                    before=end_epoch,
+                                    subreddit=subreddit,
+                                    limit=None):
+
+        if hasattr('i', 'selftext')== True:
+            selftext = i.selftext
+        else:
+            selftext = 'NaN'
+        if hasattr('i','is_original_content') == True:
+            isic = i.is_original_content
+        else:
+            isic = 'NaN'
+
         filewriter.writerow({'id': i.id,
                              'title': i.title,
                             'author': i.author,
-                            'original content': i.is_original_content,
+                            'original content': isic,
                             'selfpost': i.is_self,
                             'time created': i.created_utc,
                             'stickied': i.stickied,
                             'locked': i.locked,
                             'NSFW': i.over_18,
-                            'selftext': i.selftext,
-                            #'comment forest': cache,
+                            'selftext': selftext,
                             'number of comments': i.num_comments,
                             'score': i.score,
-                            'upvote ratio': i.upvote_ratio,
                             'permalink': i.permalink,
                             'url': i.url})
 
